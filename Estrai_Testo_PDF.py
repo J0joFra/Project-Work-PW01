3ea5316f725e503f096683a7faabefd69b095faa
Joaquim Francalanci <jofrancalanci@gmail.com>
2024-07-05 14:45:27 +0200

    Update

--- Estrai_Testo_PDF.py
+++ Estrai_Testo_PDF.py
@@ -17,10 +17,21 @@ def extract_text_from_pdf_url(pdf_url):
     else:
         return None
 
+def find_relevant_lines(text, keywords):
+    lines = text.split('\n')
+    relevant_lines = []
+    for line in lines:
+        if any(keyword in line.lower() for keyword in keywords):
+            relevant_lines.append(line)
+    return relevant_lines
+
 # Esempio di utilizzo
 pdf_url = "https://cir-reports.cir-safety.org/view-attachment/?id=94742a1a-c561-614f-9f89-14ce58abfc0b"
 pdf_text = extract_text_from_pdf_url(pdf_url)
 if pdf_text:
-    print(pdf_text)
+    keywords = ["noael", "dl50"]
+    relevant_lines = find_relevant_lines(pdf_text, keywords)
+    for line in relevant_lines:
+        print(line)
 else:
     print("Non è stato possibile estrarre il testo dal PDF.")
